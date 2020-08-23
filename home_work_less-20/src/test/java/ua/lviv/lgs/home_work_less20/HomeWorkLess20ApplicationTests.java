34bec22e11a6c0700b8f0b59a4274eac2d59a34a
LogosItAcademy <Tester99037@gmail.com>
2020-08-23 11:51:43 +0300

    home work lesson 20ja

---
+++ home_work_less-20/src/test/java/ua/lviv/lgs/home_work_less20/HomeWorkLess20ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.lgs.home_work_less20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkLess20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
